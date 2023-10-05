41603ac85d0ce55c8db880f25ecdf963cfa96255
Md. Salman Hossain <salmann.hossain@gmail.com>
2023-10-05 22:51:34 +0600

    Time: 80 ms (76.74%), Space: 14.1 MB (97.99%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,34 +1,11 @@
+from collections import Counter
+
 class Solution(object):
     def majorityElement(self, nums):
-        candidate1, candidate2 = None, None
-        count1, count2 = 0, 0
-
-        for num in nums:
-            if num == candidate1:
-                count1 += 1
-            elif num == candidate2:
-                count2 += 1
-            elif count1 == 0:
-                candidate1 = num
-                count1 = 1
-            elif count2 == 0:
-                candidate2 = num
-                count2 = 1
-            else:
-                count1 -= 1
-                count2 -= 1
-
-        count1, count2 = 0, 0
-        for num in nums:
-            if num == candidate1:
-                count1 += 1
-            elif num == candidate2:
-                count2 += 1
-
-        result = []
-        if count1 > len(nums) // 3:
-            result.append(candidate1)
-        if count2 > len(nums) // 3:
-            result.append(candidate2)
-
+        counts = Counter(nums)
+        n = len(nums)
+        threshold = n // 3
+        
+        result = [num for num, count in counts.items() if count > threshold]
+        
         return result
